d6c9a3b6bdf1417b13c3050eb38c41a5aa3ec55d
Andrej Ilibman <ilibman@protonmail.com>
2024-05-25 17:03:51 +0200

    fixes & cleanup for revision partial editor

--- frontend/src/components/RevisePartialEditor.jsx
+++ frontend/src/components/RevisePartialEditor.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useCallback } from 'react';
-import { createEditor, Editor } from 'slate';
+import { createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
 import './PartialEditor.scss';
 
 export default function RevisePartialEditor({ ...props }) {
   const [editor] = useState(() => withReact(createEditor()));
-  const [hintElement, setHintElement] = useState(null);
 
   const renderElement = useCallback((props) => {
     switch (props.element.type) {
@@ -20,60 +19,6 @@ export default function RevisePartialEditor({ ...props }) {
     return <Leaf {...props} />
   }, []);
 
-  function handleCursorChange() {
-    setHintElement(() => null);
-
-    if (!editor.selection) {
-      return false;
-    }
-
-    const selectedNode
-      = editor.selection && Editor.node(editor, editor.selection.focus);
-
-    if (selectedNode
-      && selectedNode[0].insetQuestion
-      && selectedNode[0].hint) {
-      showHintInput(selectedNode);
-    }
-  }
-
-  function showHintInput(selectedNode) {
-    // let's find the relevant inset question wrapper element
-    // for code formatted partials the structure is a bit different
-    let questionElement;
-    if (document.getElementsByClassName('active-partial')[0]
-      .childNodes[selectedNode[1][0]].className === 'code') {
-      questionElement = document.getElementsByClassName('active-partial')[0]
-        .childNodes[selectedNode[1][0]]
-        .childNodes[0]
-        .childNodes[selectedNode[1][1]];
-    } else {
-      questionElement = document.getElementsByClassName('active-partial')[0]
-        .childNodes[selectedNode[1][0]]
-        .childNodes[selectedNode[1][1]];
-    }
-
-    // get element's coordinates and stuff
-    const coords = questionElement.getBoundingClientRect();
-    const containerCoords
-      = document.getElementsByClassName('active-partial')[0]
-        .getBoundingClientRect();
-    const yDistance = coords.y - containerCoords.y;
-
-    // create the input element for the relevant hint
-    setHintElement(
-      <div
-        className="hint"
-        style={{
-          top: yDistance - 28,
-          left: coords.x - 12
-        }}
-      >
-        {selectedNode[0].hint}
-      </div>
-    );
-  }
-
   function renderPartialWithInsetQuestions(content) {
     return (
       <>
@@ -81,11 +26,15 @@ export default function RevisePartialEditor({ ...props }) {
           if (_.type === 'text') {
             return (
               <p
-                className="min-h-[27px]"
+                className={`
+                  ${props.readOnly ? 'answer-partial' : '' }
+                `}
                 key={i}
               >{_.children.map((__, j) => (
                 <span
-                  className="relative"
+                  className={`relative
+                    ${__.insetQuestion ? '' : ''}
+                  `}
                   key={i + j}
                 >
                   <span
@@ -131,15 +80,13 @@ export default function RevisePartialEditor({ ...props }) {
   
   return (
     <>
-      <div className="relative hints-container">
-        {hintElement}
-      </div>
       <div className="flex w-full">
         {props.content.some((_) => _.children.some((__) => __.insetQuestion))
           && (
             <div
               className={`w-full p-4 bg-brown-light shadow-md outline-none
                 ${props.readOnly ? '!bg-green' : '!bg-brown-light'}
+                ${props.isActivePartial ? 'active-partial' : 'inactive-partial'}
                 revise-partial-editor inactive-partial`}
             >{renderPartialWithInsetQuestions(props.content)}</div>
           )
@@ -158,7 +105,6 @@ export default function RevisePartialEditor({ ...props }) {
                 renderElement={renderElement}
                 renderLeaf={renderLeaf}
                 spellCheck={false}
-                onClick={(e) => !props.readOnly && handleCursorChange(e)}
                 onFocus={props.onPartialFocus}
               />
             </Slate>
