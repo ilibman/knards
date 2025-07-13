98891d2367298d846d4c332b9843a8630f8ba7b3
Andrej Ilibman <ilibman@protonmail.com>
2025-07-13 12:14:19 +0200

    backend/urls: using DRF router instead of path/as_view

--- backend/cards/urls.py
+++ backend/cards/urls.py
@@ -1,49 +1,10 @@
-from django.urls import path
+from rest_framework import routers
 from . import views
 
-urlpatterns = [
-    path(
-        'card-series/',
-        views.CardSeriesViewSet.as_view({'get': 'list', 'post': 'create'})
-    ),
-    path('card-series/<int:pk>/', views.CardSeriesViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    })),
-    path('tags/', views.TagsViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('tags/<int:pk>/', views.TagsViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    })),
-    path('cards/', views.CardsViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('cards/<int:pk>/', views.CardsViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    })),
-    path(
-        'card-partials/',
-        views.CardPartialsViewSet.as_view({'get': 'list', 'post': 'create'})
-    ),
-    path('card-partials/<int:pk>/', views.CardPartialsViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    })),
-    path(
-        'card-scores/',
-        views.CardScoresViewSet.as_view({'get': 'list', 'post': 'create'})
-    ),
-    path('card-scores/<int:pk>/', views.CardScoresViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    }))
-]
+router = routers.SimpleRouter()
+router.register(r'card-series', views.CardSeriesViewSet, basename='card-series')
+router.register(r'tags', views.TagsViewSet, basename='tags')
+router.register(r'cards', views.CardsViewSet, basename='cards')
+router.register(r'card-partials', views.CardPartialsViewSet, basename='card-partials')
+router.register(r'card-scores', views.CardScoresViewSet, basename='card-scores')
+urlpatterns = router.urls
