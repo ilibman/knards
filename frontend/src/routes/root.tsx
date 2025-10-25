c4f36f1d5ef9632b2d499de4e64e7f313434fd40
Andrej Ilibman <ilibman@protonmail.com>
2025-10-26 00:45:22 +0200

    rs-suite cleanup

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -3,8 +3,6 @@ import { ToastContainer } from 'react-toastify';
 import { AuthProvider} from '../context/AuthProvider';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import 'rsuite/dist/rsuite-no-reset.min.css';
-import '../styles/rs-suite-overrides.scss';
 
 export default function Root() {
   return (
